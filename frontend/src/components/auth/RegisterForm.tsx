018b98ddf091408f13665e80983086ff199c89c0
DollarP <pralod951@gmail.com>
2026-09-01 14:40:34 +0700

    feat(us1-1): manage registration form state

--- frontend/src/components/auth/RegisterForm.tsx
+++ frontend/src/components/auth/RegisterForm.tsx
@@ -1,12 +1,30 @@
+import { useState } from 'react';
+
 function RegisterForm() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    console.log({
+      name,
+      email,
+      password,
+    });
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="name">Name</label>
         <input
           id="name"
           name="name"
           type="text"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
         />
       </div>
 
@@ -16,6 +34,8 @@ function RegisterForm() {
           id="email"
           name="email"
           type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
         />
       </div>
 
@@ -25,6 +45,8 @@ function RegisterForm() {
           id="password"
           name="password"
           type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
         />
       </div>
 
